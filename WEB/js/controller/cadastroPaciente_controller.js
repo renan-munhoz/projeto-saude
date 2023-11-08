b7e080ef8828672ac79fac1edc1505ed06740ff7
renan-munhoz <renan.munhoz@edu.unifil.br>
2023-11-08 20:18:39 -0300

    upload

--- WEB/js/controller/cadastroPaciente_controller.js
+++ WEB/js/controller/cadastroPaciente_controller.js
@@ -5,65 +5,32 @@ const spawnSync = require('child_process');
 
 const router = express.Router();
 const conexao = require('../../../BANCO_DE_DADOS/banco');
-const Usuario = require('../models/usuario');
+const Paciente = require('../models/paciente');
 
-router.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "../../paginas", "cadastro-paciente.html"));
-});
-
-router.post("/", async (req, res)=>{
-    var campo_email = req.body.email;
-    var campo_senha = req.body.senha;
-
-    const usuario = await Usuario.findOne({
-        attributes: ['id', 'nome', 'email', 'senha'],
-        where: {
-            email: campo_email
-        }
-    })
-
-    if(usuario === null){
-        console.log("Usuário ou senha inválida");
-        res.sendFile(path.join(__dirname, "../../paginas", "index.html"));
-    }
-
-    if(campo_email == usuario.email && campo_senha == usuario.senha){
-        res.sendFile(path.join(__dirname, "../../paginas", "principal.html"));
-    }else{
-        console.log("Usuário ou senha inválida");
-        res.sendFile(path.join(__dirname, "../../paginas", "index.html"));
-    }
-});
-
-router.post("/cadastrarUsuarioAPI", async (req, res)=>{
-    await Usuario.create(req.body)
+router.post("/cadastrarPacienteAPI", async (req, res)=>{
+    await Paciente.create(req.body)
     .then(()=>{
        return res.json({
             erro: false,
-            mensagem: "Usuário cadastrado com sucesso!"
+            mensagem: "Paciente cadastrado com sucesso!"
        });
     }
     ).catch(()=>{
         return res.status(400).json({
             erro: true,
-            mensagem: "Falha ao cadastrar usuário!"
+            mensagem: "Falha ao cadastrar paciente!"
        });
     });
 });
 
-router.post("/cadastrarUsuario", async (req, res)=>{
+router.post("/cadastrarPaciente", async (req, res)=>{
     console.log(req.body);
-    await Usuario.create(req.body)
+    await Paciente.create(req.body)
     .then(()=>{
-        document.getElementById("popup_cadastrar_usuario").style.display = 'none';
         res.redirect('/');
     }).catch(()=>{
-        res.sendFile(path.join(__dirname, "../../paginas", "index.html"));
+        res.sendFile(path.join(__dirname, "../../", "index.html"));
     });
-});
-
-router.get("/principal", (req, res)=>{
-    res.sendFile(path.join(__dirname, "../../paginas", "principal.html"));
 });
 
 //PADRÃO SINGLETON
---
+++ WEB/js/models/paciente.js
@@ -0,0 +1,40 @@
+
+const Sequelize = require('sequelize');
+const db = require('../../../BANCO_DE_DADOS/banco');
+
+const Paciente = db.define(
+    'Paciente',{
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        nome: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        endereco: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        telefone: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        senha: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+});
+
+//COMANDO PARA CRIAR A TABELA NO BANCO DE DADOS
+Paciente.sync();
+
+//VERIFICA SE TEM ALGUMA ALTERAÇÃO NA TABELA PARA INSERIR OS NOVOS CAMPOS
+//Usuario.sync({alter: true});
+module.exports = Paciente;
