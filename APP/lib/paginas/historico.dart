808d29ca4be61c37300a6984fc203515e8f3f357
LuisFelipeZuin <100451051+LuisFelipeZuin@users.noreply.github.com>
2023-11-09 14:31:11 -0300

    Update historico.dart

---
+++ APP/lib/paginas/historico.dart
@@ -0,0 +1,61 @@
+import 'package:appclinica/paginas/main.dart';
+import 'package:flutter/material.dart';
+
+
+
+class Tabela extends StatefulWidget{
+
+@override
+  TabelaState createState() => TabelaState();
+
+}
+
+class TabelaState extends State<Tabela>{
+
+@override
+
+ Widget build (BuildContext context) => Scaffold(
+
+   appBar: AppBar(
+    title: Text(MyApp.title),
+   ),
+    body:Center(
+     child:Table(
+      border: TableBorder.all(),
+      columnWidths: {
+        0:FractionColumnWidth(0.5),
+        1:FractionColumnWidth(0.25),
+        2:FractionColumnWidth(0.25),
+      },
+      children:[
+        buildRow(['Nome','Data','Hora','Médico'],isHeader:true),
+        buildRow(['1','2','3','4']),
+        buildRow(['1','2','3','4']),
+        buildRow(['1','2','3','4']),
+        buildRow(['1','2','3','4']),
+        buildRow(['1','2','3','4']),
+        buildRow(['1','2','3','4']),
+        buildRow(['1','2','3','4']),
+        buildRow(['1','2','3','4']),
+        buildRow(['1','2','3','4']),
+        buildRow(['1','2','3','4']),
+      ]
+     ),
+    )
+  );
+
+  TableRow buildRow(List<String> cells,{bool isHeader = false})=> TableRow(
+    children: cells.map((cell){
+
+     final style = TextStyle(
+      fontWeight: isHeader? FontWeight.bold : FontWeight.normal,
+      fontSize:18,
+     );
+
+      return Padding(
+        padding: const EdgeInsets.all(12),
+        child: Center(child:Text(cell,style:style)),
+      );
+    }).toList(),
+  );
+}
