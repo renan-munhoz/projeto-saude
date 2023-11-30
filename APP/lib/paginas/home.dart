5ef0616a1466e5780c00854783b042bcc47d9640
LuisFelipeZuin <100451051+LuisFelipeZuin@users.noreply.github.com>
2023-11-30 14:30:36 -0300

    Update home.dart

--- APP/lib/paginas/home.dart
+++ APP/lib/paginas/home.dart
@@ -1,50 +1,84 @@
 
+import 'package:appclinica/paginas/agendamento.dart';
+import 'package:appclinica/paginas/historico.dart';
+import 'package:appclinica/paginas/loginfuncionario.dart';
+import 'package:appclinica/paginas/loginpaciente.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: GeneralMenuScreen(),
+    );
+  }
+}
+
+class GeneralMenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Center(
-          child: SizedBox(
-            width: 100,
-            child: Image.asset("assets/app-logo.png"),
-          ),
-        ),
-        leading: Container(
-          height: 40,
-          width: 40,
-          decoration: new BoxDecoration(
-            image: new DecorationImage(
-              image: AssetImage("assets/user-picture.png"),
-              fit: BoxFit.fitHeight,
-            ),
-          ),
-        ),
-        actions: <Widget>[
-          Container(
-            width: 60,
-            child: FlatButton(
-              child: Icon(
-                Icons.search,
-                color: Color(0xFFBABABA),
-              ),
-              onPressed: () => {},
-            ),
-          ),
-        ],
+        title: Text('Menu Geral'),
       ),
-      body: Container(
-        color: Color(0xFFF2F3F6),
-        child: ListView(
-          children: <Widget>[
-            cardItem(),
-            cardItem(),
-            cardItem(),
-            cardItem(),
-            cardItem(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Navegar para a tela de agendamento
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Agendamento()),
+                );
+              },
+              child: Text('Agendamento'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Navegar para a tela de histórico
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Tabelahistorico()),
+                );
+              },
+              child: Text('Histórico'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Navegar para a tela Sobre Nós
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AboutUsScreen()),
+                );
+              },
+              child: Text('Sobre Nós'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Navegar para a tela de login de funcionário
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => loginfuncionario()),
+                );
+              },
+              child: Text('Login de Funcionário'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Navegar para a tela de login de paciente
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => loginpaciente()),
+                );
+              },
+              child: Text('Login de Paciente'),
+            ),
           ],
         ),
       ),
@@ -52,43 +86,72 @@ class HomePage extends StatelessWidget {
   }
 }
 
-Widget cardItem() {
-  return Card(
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        const ListTile(
-          leading: CircleAvatar(
-            backgroundImage: NetworkImage(
-                "https://baltaio.blob.core.windows.net/student-images/1edd5c50-bae9-11e8-8eb4-39de303632c1.jpg"),
-          ),
-          title: Text("Bruce Wayne"),
-          subtitle: Text("09/05/2019 18:37"),
-          trailing: Icon(Icons.more_vert),
-        ),
-        Container(
-          child: Image.asset("assets/post-picture-001.png"),
-        ),
-        Container(
-          padding: EdgeInsets.all(10),
-          child: Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis ex sem. Praesent elit dui, iaculis at interdum eu, rutrum et mi. "),
-        ),
-        ButtonTheme.bar(
-          child: ButtonBar(
-            children: <Widget>[
-              FlatButton(
-                child: Icon(Icons.favorite),
-                onPressed: () {},
-              ),
-              FlatButton(
-                child: Icon(Icons.share),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
-      ],
-    ),
-  );
+class AppointmentScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Agendamento'),
+      ),
+      body: Center(
+        child: Text('Conteúdo da Tela de Agendamento'),
+      ),
+    );
+  }
+}
+
+class HistoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Histórico'),
+      ),
+      body: Center(
+        child: Text('Conteúdo da Tela de Histórico'),
+      ),
+    );
+  }
+}
+
+class AboutUsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sobre Nós'),
+      ),
+      body: Center(
+        child: Text('Conteúdo da Tela Sobre Nós'),
+      ),
+    );
+  }
+}
+
+class EmployeeLoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login de Funcionário'),
+      ),
+      body: Center(
+        child: Text('Conteúdo da Tela de Login de Funcionário'),
+      ),
+    );
+  }
+}
+
+class PatientLoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login de Paciente'),
+      ),
+      body: Center(
+        child: Text('Conteúdo da Tela de Login de Paciente'),
+      ),
+    );
+  }
 }
